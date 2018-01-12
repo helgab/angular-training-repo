5ab638e05e54070599fb5e93a7b4edc4a2dca398
Helga Paulger <helga.bucur@gmail.com>
2018-01-13 00:56:53 +0200

    assignment 7

--- src/app/movies/movie.component.ts
+++ src/app/movies/movie.component.ts
@@ -6,31 +6,68 @@ import { Component } from "@angular/core";
     styleUrls: ['./movie.component.css']
 })
 export class MovieComponent {
-    public title = "Star Wars: The Last Jedi";
-    public year = "(2017)";
-    public duration = "152 min";
-    public genre = "Action, Adventure, Fantasy";
-    public plot = "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.";
-    public poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg";
-    public isLiked = false;
-    public comments = "enter comment...";
-    public isSaved = false;
+    public movies = [
+        {
+          title: 'Star Wars: The Last Jedi',
+          year: 2017,
+          duration: '152 min',
+          genre: 'Action, Adventure, Fantasy',
+          plot: 'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.',
+          poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg',
+          isLiked: false,
+          comments: "enter comment...",
+          isSaved: false
+        },
+        {
+          title: 'Black Swan',
+          year: 2010,
+          duration: '108 min',
+          genre: 'Drama, Thriller',
+          plot: 'A committed dancer wins the lead role in a production of Tchaikovskys \"Swan Lake\" only to find herself struggling to maintain her sanity.',
+          poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BNzY2NzI4OTE5MF5BMl5BanBnXkFtZTcwMjMyNDY4Mw@@._V1_SX300.jpg',
+          isLiked: false,
+          comments: "enter comment...",
+          isSaved: false
+        },
+        {
+          title: 'Fight Club',
+          year: 1999,
+          duration: '139 min',
+          genre: 'Drama',
+          plot: 'An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soapmaker, forming an underground fight club that evolves into something much, much more.',
+          poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzFjMWNhYzQtYTIxNC00ZWQ1LThiOTItNWQyZmMxNDYyMjA5XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+          isLiked: false,
+          comments: "enter comment...",
+          isSaved: false
+        },
+        {
+          title: 'The Godfather: Part II',
+          year: 1974,
+          duration: '202 min',
+          genre: 'Crime, Drama',
+          plot: 'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.',
+          poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjZiNzIxNTQtNDc5Zi00YWY1LThkMTctMDgzYjY4YjI1YmQyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
+          isLiked: false,
+          comments: "enter comment...",
+          isSaved: false
+        }
+      ];
 
-    like() {
-        this.isLiked = !this.isLiked;
+    like(index) {
+        this.movies[index].isLiked = !this.movies[index].isLiked;
     }
 
-    clear() {
-        this.comments = "";
+    clear(index) {
+        this.movies[index].comments = "";
     }
 
-    saveToggle() {
-        this.isSaved = !this.isSaved;
+    saveToggle(index) {
+        this.movies[index].isSaved = !this.movies[index].isSaved;
     }
 
-    getWordsNumber(){
-        if(this.comments.length != 0) {
-            return this.comments.split(" ").length;
+    getWordsNumber(index){
+        if(this.movies[index].comments.length != 0) {
+            return this.movies[index].comments.split(" ").length;
         }
         else return 0;
     }
